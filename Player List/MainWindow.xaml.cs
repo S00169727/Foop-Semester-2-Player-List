acfdc9e3233a168cf7335070864d3f58d00e73e8
Conor Fearon <S00169727@mail.itsligo.ie>
2018-02-01 22:53:55 +0000

    Add project files.

---
+++ Player List/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Player_List
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Fields
+        private Random _dateGen = new Random();
+        private const int _startAge = 8;
+        private const int _endAge = 18;
+
+        //Creating a list of player objects
+        List<Player> playersList = new List<Player>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            //Filling the list with player objects
+            playersList.Add(new Player() { Name = "Mike Smith", DOB = GetRandomDate(_startAge, _endAge, _dateGen) });
+            playersList.Add(new Player() { Name = "Sue Smith", DOB = GetRandomDate(_startAge, _endAge, _dateGen) });
+            playersList.Add(new Player() { Name = "John Smith", DOB = GetRandomDate(_startAge, _endAge, _dateGen) });
+            playersList.Add(new Player() { Name = "Joanne Smith", DOB = GetRandomDate(_startAge, _endAge, _dateGen) });
+
+            //Calculating the age for every player in the list
+            foreach (Player player in playersList)
+            {
+                player.CalculateAge();
+                player.CalculateAgeGroup();
+            }
+            
+
+            //Setting the list box to display the player objects once the application loads
+            listBoxPlayerList.ItemsSource = playersList;
+        }
+
+        
+        //Method to return a random date of birth between 18 and 8 years ago
+        static DateTime GetRandomDate(int _startAgeIn, int _endAgeIn, Random _dateGenIn)
+        {
+            //Get start date and end date
+            DateTime startDate = DateTime.Now.AddYears(-_startAgeIn); //31/01/2008
+            DateTime endDate = DateTime.Now.AddYears(-_endAgeIn); //31/01/2000
+
+            //Get a randomnumber of days in that range
+            TimeSpan range = startDate - endDate;
+
+            //Grab the total number of the days from the year range
+            double daysInRange = range.TotalDays;
+
+            int randomDays = _dateGenIn.Next(0,(int)daysInRange);
+
+            //Getting a new random date
+            DateTime newRandomDate = endDate.AddDays(randomDays);
+
+            return newRandomDate;
+        }
+
+
+        private void BtnAddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            //Checking to see if the name and datepicker have a value inside of them
+            if (txtBoxName != null && datePicker1 != null)
+            {
+                //If they have data in them then add a new player object to the list and display it
+                playersList.Add(new Player() { Name = txtBoxName.Text, DOB = DateTime.ParseExact(datePicker1.SelectedDate.Value.ToString(),"dd/MM/yyyy", null) });
+            }
+            //Make the listbox display whatever is in it at the time before the button press
+            else
+            {
+                listBoxPlayerList.ItemsSource = null;
+                listBoxPlayerList.ItemsSource = playersList;
+            }
+
+            //Keeping the orignal list if nothing is put into the boxes originally
+            listBoxPlayerList.ItemsSource = null;
+            listBoxPlayerList.ItemsSource = playersList;
+        }
+
+        private void BtnSaveContent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnLoadContent_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Player List/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Player_List
+{
+    class Player
+    {
+        //Enum for age groups
+        private enum _AgeGroups { u10, u12, u14, u16, u18};
+
+        //Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime DOB { get; set; }
+        public string AgeGroupsOutput { get; set; }
+
+        //Player method sets the age based on todays date minus the random age set
+        public void CalculateAge()
+        {
+            Age = DateTime.Now.Year - DOB.Year;
+        }
+
+        //Method to give the players their age group
+        public void CalculateAgeGroup()
+        {
+            if (Age <= 10)
+            {
+                AgeGroupsOutput = _AgeGroups.u10.ToString();
+            }
+            else if (Age > 10 && Age <= 12)
+            {
+                AgeGroupsOutput = _AgeGroups.u12.ToString();
+            }
+            else if (Age > 12 && Age <= 14)
+            {
+                AgeGroupsOutput = _AgeGroups.u14.ToString();
+            }
+            else if (Age > 14 && Age <= 16)
+            {
+                AgeGroupsOutput = _AgeGroups.u16.ToString();
+            }
+            else if (Age > 16 && Age <= 18)
+            {
+                AgeGroupsOutput = _AgeGroups.u18.ToString();
+            }
+        }
+       
+
+        //Overriding the ToString method
+        public override string ToString()
+        {
+            return string.Format($"{Name}, {Age}, {AgeGroupsOutput}");
+        }
+
+    }
+}
